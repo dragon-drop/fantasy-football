2cba903c5a1fac3f52f32a008acba28e0b65b411
Adam Dawkins <adam@dragondrop.uk>
2017-02-17 12:38:29 +0000

    Seed players from http://api.football-data.org/documentation

--- app/server/server.js
+++ app/server/server.js
@@ -5,6 +5,7 @@ import { typeDefs } from '/imports/api/schema';
 import { resolvers } from '/imports/api/resolvers';
 import { Meteor } from 'meteor/meteor';
 import Fantasy from '/imports/fantasy';
+import { Players } from '/imports/infrastructure/collections';
 
 const schema = makeExecutableSchema({
   typeDefs,
@@ -13,4 +14,39 @@ const schema = makeExecutableSchema({
 
 createApolloServer({
   schema,
+});
+
+Meteor.startup(() => {
+  if (Players.find().count() === 0) {
+    console.log('There are no players, seed the db');
+    const { players } = JSON.parse(Assets.getText('players.json'));
+    
+    const positionsMap = {
+      'Central Midfield': 'MD',
+      'Centre Forward': 'FW',
+      'Centre Back': 'DF',
+      'Attacking Midfield': 'MD',
+      'Keeper': 'GK',
+      'Left-Back': 'DF',
+      'Right-Back': 'DF',
+      'Defensive Midfield': 'MD',
+      'Central Midfield': 'MD',
+      'Attacking Midfield': 'MD',
+      'Left Wing': 'MD',
+      'Secondary Striker': 'FW',
+    };
+    players.forEach((player) => {
+      const { name, id, jerseyNumber } = player;
+      const position = positionsMap[player.position]; 
+
+      return Fantasy.PlayerRepo.insert(new Fantasy.Player({
+        _id: `${id}`,
+        name,
+        position,
+        price: jerseyNumber,
+      }));
+
+    });
+
+  }
 });
